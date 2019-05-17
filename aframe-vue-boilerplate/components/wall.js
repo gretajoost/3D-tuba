b7331884240385016223c50414103fb7f0863494
gretajoost <46886691+gretajoost@users.noreply.github.com>
2019-05-17 11:38:52 +0300

    3D tuba

---
+++ aframe-vue-boilerplate/components/wall.js
@@ -0,0 +1,8 @@
+export default {
+    
+    template: `
+    <a-entity class="wall" >
+         <a-plane  position="0 2 0" width="10" height="4" class="wall" color="salmon"></a-plane>
+         </a-entity>
+    `
+}
